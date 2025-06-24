354f5991bc9bec54d97813c67f40e1e9f3bdbbba
Shawn Mayol <mayolshawn@gmail.com>
2025-06-25 01:31:56 +0800

    feat: set up Firebase configuration and routing with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
 
-    return (
-        <>
-           
-        </>
-    );
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  );
 }
 
 export default App;
