d6d8245fadad1526791ae5d587d4231e71f883ed
Shawn Mayol <mayolshawn@gmail.com>
2025-06-27 12:59:33 +0800

    fix: browser open on start

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
+        open: true,
         port: 3000,
     },
 });
