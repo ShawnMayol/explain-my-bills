bb94db7f3c37520e7762440d0582576faf65c59a
Shawn Mayol <mayolshawn@gmail.com>
2025-06-28 13:22:37 +0800

    feat: update LandingPage UI

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -23,28 +23,37 @@ export default function LandingPage() {
     }
 
     return (
-        <div className="h-screen w-screen items-center flex flex-col justify-center">
-            <div>
-                <p className="text-2xl mb-2 text-start">
-                    {user
-                        ? `Welcome Back, ${user.displayName}!`
-                        : "Welcome!"}
+        <div className="relative h-screen w-screen flex flex-col items-center justify-center bg-[#1B1C21] text-white overflow-hidden">
+            <div className="absolute right-1/5 top-1/6 w-[450px] h-[450px] rounded-full bg-gray-100 opacity-10 blur-3xl pointer-events-none z-0"></div>
+            {/* <div className="absolute left-1/5 bottom-1/6 w-[250px] h-[250px] rounded-full bg-gray-100 opacity-10 blur-3xl pointer-events-none z-0"></div> */}
+            
+            <div className="z-10">
+                <p className="text-2xl mb-2 text-start font-semibold">
+                    <span className="text-yellow-300">
+                        {user ? "Welcome Back" : "Welcome"}
+                    </span>
+                    {user && (
+                        <span className="text-white">{`, ${user.displayName}!`}</span>
+                    )}
+                    {!user && <span className="text-white">!</span>}
                 </p>
-                <h1 className="text-7xl font-bold mb-14">Explain My Bills!</h1>
+                <h1 className="text-7xl font-bold mb-14">
+                    Explain My Bills!
+                </h1>
             </div>
 
-            <div>
+            <div className="z-10">
                 {user ? (
                     <>
                         <Link
                             to="/dashboard"
-                            className="rounded-xl p-3 text-lg border-2 font-semibold mr-10 px-12 hover:cursor-pointer"
+                            className="rounded-xl p-3 text-lg border-2 border-white text-white font-semibold mr-10 px-12 hover:bg-gray-100 hover:text-black transition"
                         >
                             Dashboard
                         </Link>
                         <button
                             onClick={handleLogout}
-                            className="rounded-xl p-3 text-lg border-2 font-semibold px-12 hover:cursor-pointer"
+                            className="rounded-xl p-3 text-lg border-2 border-white text-white font-semibold px-12 hover:bg-gray-100 hover:text-black transition hover:cursor-pointer"
                         >
                             Logout
                         </button>
@@ -53,13 +62,13 @@ export default function LandingPage() {
                     <>
                         <Link
                             to="/signup"
-                            className="rounded-xl p-3 text-lg border-2 font-semibold mr-10 px-12 hover:cursor-pointer"
+                            className="rounded-xl p-3 text-lg border-2 border-white text-white font-semibold mr-10 px-12 hover:bg-gray-100 hover:text-black transition"
                         >
                             Sign Up
                         </Link>
                         <Link
                             to="/signin"
-                            className="rounded-xl p-3 text-lg border-2 font-semibold px-12 hover:cursor-pointer"
+                            className="rounded-xl p-3 text-lg border-2 border-white text-white font-semibold px-12 hover:bg-gray-100 hover:text-black transition"
                         >
                             Sign In
                         </Link>
