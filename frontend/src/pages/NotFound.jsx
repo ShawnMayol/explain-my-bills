5cebbbdc0486fddfbe915508f33acd510e885348
Shawn Mayol <mayolshawn@gmail.com>
2025-06-28 13:22:10 +0800

    feat: update NotFound page UI

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -5,12 +5,12 @@ export default function NotFound() {
     const navigate = useNavigate();
 
     return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center bg-white">
-            <h1 className="text-6xl font-bold mb-6 text-gray-800">404</h1>
-            <p className="text-2xl mb-8 text-gray-600">Page Not Found</p>
+        <div className="h-screen w-screen flex flex-col items-center justify-center bg-[#1B1C21] text-white">
+            <h1 className="text-6xl font-bold mb-6 text-yellow-300 drop-shadow-lg">404</h1>
+            <p className="text-2xl mb-8 text-gray-300">Page Not Found</p>
             <button
-                onClick={() => navigate("/")}
-                className="px-8 py-3 border-2 font-semibold rounded-xl text-lg hover:cursor-pointer"
+                onClick={() => navigate("/Dashboard")}
+                className="px-8 py-3 border-2 border-white font-semibold rounded-xl text-lg text-white hover:bg-gray-100 hover:text-black transition hover:cursor-pointer"
             >
                 Back to Home
             </button>
