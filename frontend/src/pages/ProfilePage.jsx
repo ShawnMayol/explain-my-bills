7a0f5cc3f5631cf15fc60db30b56d1e08f4153ff
Shawn Mayol <mayolshawn@gmail.com>
2025-07-06 01:36:52 +0800

    refactor: add left margin for ProfilePage

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -22,7 +22,6 @@ export default function ProfilePage() {
         <div className="flex h-screen w-screen bg-[#1B1C21] text-white overflow-hidden relative">
             <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
-            {/* Top Bar Mobile */}
             <div className="absolute top-0 left-0 right-0 z-30 md:hidden bg-black/10 flex items-center h-12 px-4 py-7">
                 <button
                     className="text-yellow-300 hover:text-white cursor-pointer ps-5"
@@ -32,8 +31,8 @@ export default function ProfilePage() {
                 </button>
             </div>
 
-            <div className="flex-1 flex flex-col items-center justify-center">
-                <div className="flex flex-col items-center gap-6 w-full max-w-[350px]">
+            <div className="md:ml-[20%] flex-1 flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center gap-6 w-full max-w-[550px] px-10">
                     <Link
                         to="/profile/edit-username"
                         className="w-full py-3 border-2 border-white rounded-full font-semibold text-white hover:bg-yellow-300 hover:text-black transition text-center"
