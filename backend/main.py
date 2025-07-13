8c846099cd391db4706bf0dbc3f86b50c31cb26a
Nash Adam Munoz <150409949+lichclass@users.noreply.github.com>
2025-07-13 23:04:54 +0800

    fix: added vercel as origins for backend

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,14 @@ from routers import bills, test, upload, delete
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://explain-my-bills.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
